5bacd7f92869f84614cb0eab85579318b24eb27f
mrazmartin <mraz.martin.cz@gmail.com>
2023-10-29 12:54:28 +0100

    clean up

--- gpt/chat_prompt.py
+++ gpt/chat_prompt.py
@@ -1,12 +1,11 @@
 # this is the best of Jason M. Mráz ever
 
-# key: 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
+# key: sk-z4lDmSwbaJZaEIec90qMT3BlbkFJ8cS5Og3SXp90XzUqgMy   O
 
 import openai
 import os
 from dotenv import load_dotenv, find_dotenv
 
-DEFAULT_CONTEXT = ""
 
 JSON_STYLE = """Provide the results in the following json format:
     "question": "What is the key difference between Python 2 and Python 3?",
@@ -24,12 +23,9 @@ class gpt_api:
     def __init__(self, key=None) -> None:
         self.model = "gpt-3.5-turbo"
         self.debug_mode = False
-        self.context = DEFAULT_CONTEXT
 
         if key:
             self.key = key
-
-        # _ = load_dotenv(find_dotenv())
 
     def read_key_from_file(self, file_name="api_key.txt"):
         with open(file_name, 'r') as file:
@@ -72,30 +68,12 @@ class gpt_api:
         return 
 
 
-
 ### example run
-
 def example_run():
     tmp_api = gpt_api("catch me if you can")
     tmp_api.read_key_from_file("api_key.txt")
     tmp_api.debug(True)
     tmp_api.json_style(JSON_STYLE)
     tmp_api.prompt(text="Give me two true or false coding question answer pairs based on python, return it in json format, keep the difficulty level on very hard, provide examples for each question")
-    """
-    {
-    "questions": [
-        {
-        "question": "True or False: In Python, the 'is' operator compares the values of two objects.",
-        "answer": "False",
-        "example": "x = [1, 2, 3]\ny = [1, 2, 3]\nprint(x is y)  # Output: False"
-        },
-        {
-        "question": "True or False: Python supports tail call optimization (TCO) for recursive functions.",
-        "answer": "False",
-        "example": "import sys\nsys.setrecursionlimit(10**6)\n\ndef factorial(n, acc=1):\n    if n == 0:\n        return acc\n    return factorial(n-1, n*acc)\n\nprint(factorial(1000))  # Output: RecursionError: maximum recursion depth exceeded in comparison"
-        }
-    ]
-    }
-    """
 
 # example_run()
